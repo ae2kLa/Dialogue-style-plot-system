7ea81818b921cc99eb4bcb1321fdb810acc8d2fc
AshScops <1349143251@qq.com>
2023-01-07 23:03:11 +0800

    AddPredicate

---
+++ Assets/Scripts/CommandReceiver.cs
@@ -0,0 +1,95 @@
+using FairyGUI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEditor.Progress;
+
+
+namespace plot
+{
+    public class CommandReceiver : MonoSingleton<CommandReceiver>
+    {
+        private GComponent dialogueRoot;
+        private List<int> decisions = new List<int>();
+        private float typingEffectTimeDevision = 0.01f;
+
+        protected void Awake()
+        {
+            dialogueRoot = GetComponent<UIPanel>().ui;
+            dialogueRoot.MakeFullScreen();
+            Debug.Log("EventReciver Instance Awake Done");
+        }
+
+        protected override void OnStart()
+        {
+            GLoader loader = dialogueRoot.GetChild("n4").asLoader;
+            //TODO:和设置屏幕分辨率相关联
+            loader.SetSize(1920, 1080);
+        }
+
+        public IEnumerator HEADER(string title, bool is_skippable, string fit_mode)
+        {
+            yield return null;
+            Debug.Log("HEADER Done!");
+        }
+
+        public IEnumerator Background(string image = "white")
+        {
+            GLoader loader = dialogueRoot.GetChild("n4").asLoader;
+            loader.url = image;
+            yield return null;
+            Debug.Log("Background Done!");
+        }
+
+        public IEnumerator BackgroundClose(float time = 2)
+        {
+            yield return new WaitForSeconds(time);
+            Debug.Log("Background Close!");
+        }
+
+        public IEnumerator Dialogue(string name = "", string text = "")
+        {
+            GTextField gtf = dialogueRoot.GetChild("n0").asTextField;
+            gtf.text = name;
+            gtf = dialogueRoot.GetChild("n1").asTextField;
+            gtf.text = text;
+            TypingEffect typingEffect = new TypingEffect(gtf);
+            typingEffect.Start();
+            typingEffect.PrintAll(typingEffectTimeDevision);
+            yield return new WaitForMouseButtonDown(1);
+            Debug.Log("Dialogue Done!");
+        }
+
+        public IEnumerator Delay(float time)
+        {
+            yield return new WaitForSeconds(time);
+            Debug.Log("Delay Done!");
+        }
+
+
+
+        public IEnumerator Decision(string options, string values)
+        {
+            //等待点击按钮
+            yield return new WaitForMouseButtonDown(1);
+            Debug.Log("Decision Done!");
+        }
+
+        public IEnumerator Predicate()
+        {
+            yield return new WaitForMouseButtonDown(1);
+            Debug.Log("Predicate Done!");
+        }
+
+        public IEnumerator CameraShake(double duration, double xstrength, double ystrength, int vibrato, int randomness, bool fadeout)
+        {
+            yield return null;
+            Debug.Log("CameraShake Done!");
+        }
+
+
+
+
+    }
+
+}
---
+++ Assets/Scripts/CommandSender.cs
@@ -0,0 +1,130 @@
+using plot;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using Unity.VisualScripting;
+using UnityEditor.Timeline.Actions;
+using UnityEngine;
+
+namespace plot
+{
+    public class CommandSender : MonoSingleton<CommandSender>
+    {
+        public string filePath = "Assets/Text/level01.txt";
+        private TextParser textParser;
+        public SenderState senderState = SenderState.KeepGoOn;
+        public int targetChoice = -1;
+        private void Awake()
+        {
+            try
+            {
+                textParser = new TextParser(filePath);
+            }
+            catch (IOException ex)
+            {
+                UnityEngine.Debug.LogError("Error opening file: " + ex.Message);
+            }
+        }
+
+        private void Start()
+        {
+            StartCoroutine(ExecuteAllCommands());
+        }
+
+
+        #region old function
+        //IEnumerator ExecuteAllCommands()
+        //{
+        //    MyCommand[] mc = textParser.ParserByLine();
+
+        //    for (int i = 0; i < mc.Length; i++)
+        //    {
+        //        Type type = Type.GetType("plot." + mc[i].name);
+        //        object obj = Activator.CreateInstance(type);
+        //        Dictionary<string, object> parameters = textParser.ParseParameters(mc[i].parameter);
+
+        //        foreach (var p in parameters)
+        //        {
+        //            PropertyInfo property = type.GetProperty(p.Key);
+        //            property.SetValue(obj, Convert.ChangeType(p.Value, property.PropertyType));
+        //        }
+
+        //        MethodInfo m = type.GetMethod("Execute", BindingFlags.NonPublic | BindingFlags.Instance);
+        //        m.Invoke(obj, null);
+        //        yield return new WaitForSeconds(1);
+        //    }
+        //}
+        #endregion
+
+        #region new function
+        IEnumerator ExecuteAllCommands()
+        {
+            MyCommand[] mc = textParser.ParserByLine();
+            Type type = Type.GetType("plot.CommandReceiver");
+
+            for (int i = 0; i < mc.Length; i++)
+            {
+                if(senderState == SenderState.FindPredicate)
+                {
+                    //KeepGoOn
+                    if (mc[i].name.Equals("PredicateEnd"))
+                    {
+                        senderState = SenderState.KeepGoOn;
+                    }
+                    else if(mc[i].name.Equals("Predicate"))
+                    {
+                        Dictionary<string, object> ps = textParser.ParseParameters(mc[i].parameter);
+                        int choice = (int)Convert.ChangeType(ps["value"], Type.GetType("int"));
+                        if (choice == targetChoice)
+                        {
+                            senderState = SenderState.KeepGoOn;
+                        }
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+
+                MethodInfo method = type.GetMethod(mc[i].name, BindingFlags.Public | BindingFlags.Instance);
+                ParameterInfo[] paramInfo = method.GetParameters();
+                object[] invokeParams = new object[paramInfo.Length];
+
+                Dictionary<string, object> parameters = textParser.ParseParameters(mc[i].parameter);
+
+                //生成形参
+                for (int j = 0; j < paramInfo.Length; j++)
+                {
+                    if (parameters.ContainsKey(paramInfo[j].Name))
+                    {
+                        invokeParams[j] = Convert.ChangeType(parameters[paramInfo[j].Name], paramInfo[j].ParameterType);
+                    }
+                    else if (paramInfo[j].HasDefaultValue)
+                    {
+                        invokeParams[j] = paramInfo[j].DefaultValue;
+                    }
+                    else
+                    {
+                        UnityEngine.Debug.LogError("Function has no default parameter!");
+                    }
+                }
+
+                yield return StartCoroutine((IEnumerator)method.Invoke(CommandReceiver.Instance, invokeParams));
+            }
+        }
+        #endregion
+
+
+
+    }
+
+    public enum SenderState
+    {
+        KeepGoOn = 0,
+        FindPredicate,
+        EndPredicate
+    };
+
+}
